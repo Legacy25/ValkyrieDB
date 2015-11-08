92b66500b89d3e233ffaddbf0f6ed90816dfa37a
kaushgem <kaushcs@gmail.com>
2015-11-08 12:52:04 -0500

    evaluate() in binary expression

--- Valkyrie/Expression/BianryExpression.h
+++ Valkyrie/Expression/BianryExpression.h
@@ -15,6 +15,7 @@ public:
     void setRightExpression(Expression*);
 
     virtual Value* getValue() = 0;
+    virtual Value* evaluate() = 0;
 };
 
 //Arithmetic Operators
@@ -22,6 +23,7 @@ class Addition: public BinaryExpression {
     Operations operation = Operations.ADDITION;
 public:
     Value* getValue();
+    Value* evaluate();
 };
 
 
@@ -29,18 +31,21 @@ class Subtraction: public BinaryExpression {
     Operations operation = Operations.SUBTRACTION;
 public:
     Value* getValue();
+    Value* evaluate();
 };
 
 class Multiplication: public BinaryExpression {
     Operations operation = Operations.MULTIPLICATION;
 public:
     Value* getValue();
+    Value* evaluate();
 };
 
 class Division: public BinaryExpression {
     Operations operation = Operations.DIVISION;
 public:
     Value* getValue();
+    Value* evaluate();
 };
 
 //Logical Operators
@@ -54,42 +59,49 @@ class Or: public BinaryExpression {
     Operations operation = Operations.OR;
 public:
     Value* getValue();
+    Value* evaluate();
 };
 
 class Equal: public BinaryExpression {
     Operations operation = Operations.EQUAL;
 public:
     Value* getValue();
+    Value* evaluate();
 };
 
 class NotEqual: public BinaryExpression {
     Operations operation = Operations.NOT_EQUAL;
 public:
     Value* getValue();
+    Value* evaluate();
 };
 
 class LessThan: public BinaryExpression {
     Operations operation = Operations.LESS_THAN;
 public:
     Value* getValue();
+    Value* evaluate();
 };
 
 class LessThanEqual: public BinaryExpression {
     Operations operation = Operations.LESS_THAN_EQUAL;
 public:
     Value* getValue();
+    Value* evaluate();
 };
 
 class GreaterThan: public BinaryExpression {
     Operations operation = Operations.GREATER_THAN;
 public:
     Value* getValue();
+    Value* evaluate();
 };
 
 class GreaterThanEqual: public BinaryExpression {
     Operations operation = Operations.GREATER_THAN_EQUAL;
 public:
     Value* getValue();
+    Value* evaluate();
 };
 
 
